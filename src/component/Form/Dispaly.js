de5061fdb41d1d12056c6a354c61a1214500f1dd
Ace <kronicrespawn@gmail.com>
2023-02-17 10:06:20 -0600

    style added

--- src/component/Form/Dispaly.js
+++ src/component/Form/Dispaly.js
@@ -30,3 +30,38 @@ export default function Display({ route }) {
     )
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'grey',
+        paddingLeft: 40,
+        paddingRight: 40,
+        paddingTop: 40
+    },
+    header: {
+        marginBottom: 20,
+        alignSelf: 'stretch'
+    },
+    details: {
+        marginBottom: 15,
+    },
+    headerText: {
+        fontSize:  24,
+        color: '#fff',
+        borderBottomColor: '#199187',
+        paddingBottom: 10,
+        borderBottomWidth: 1
+    },
+    titleText: {
+        fontWeight: 'bold',
+        color: 'yellow',
+        fontSize: 15,
+        marginBottom: 10
+    },
+    key: {
+        fontWeight: 'bold'
+    },
+    text: {
+        color: '#d3d3d3',
+    }
+});
