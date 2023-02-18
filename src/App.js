1300e4a293e12bbc5dc306ee199b483340ddc1c8
Ace <kronicrespawn@gmail.com>
2023-02-17 23:00:32 -0600

    updated

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import ResumeForm from "./component/Form/cvForm";
 import Display from "./component/Form/Display";
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
+const Stack = createNativeStackNavigator()
 class App extends Component {
   render() {
     return (<div>
           <h1>CV : Resume Creator</h1>
           <ResumeForm />
+        
           </div>
     )
   }
@@ -17,3 +19,21 @@ class App extends Component {
 export default App
 
 
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen 
+//           name="Resume"
+//           component = {ResumeForm}
+//         />
+
+//         <Stack.Screen 
+//           name="ShowCV"
+//           component = {Display}
+//         />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
