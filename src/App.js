968c9910c82dd64534b73da888534b469664acf0
Ace <kronicrespawn@gmail.com>
2023-02-22 17:04:43 -0600

    commented out unused (temp)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 // import * as React from 'react';
 import ResumeForm from "./component/Form/cvForm";
-import Display from "./component/Form/Display";
-import { NavigationContainer } from "@react-navigation/native"
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
-const Stack = createNativeStackNavigator()
+// import Display from "./component/Form/Display";
+// import { NavigationContainer } from "@react-navigation/native"
+// import { createNativeStackNavigator } from "@react-navigation/native-stack"
+// const Stack = createNativeStackNavigator()
 class App extends Component {
   render() {
     return (<div>
@@ -20,20 +20,3 @@ export default App
 
 
 
-// export default function App() {
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator>
-//         <Stack.Screen 
-//           name="Resume"
-//           component = {ResumeForm}
-//         />
-
-//         <Stack.Screen 
-//           name="ShowCV"
-//           component = {Display}
-//         />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// }
--- src/component/Form/Display.js
+++ src/component/Form/Display.js
@@ -1,67 +1,34 @@
-import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-// import { useState } from 'react';
+import { useState } from 'react';
 
-export default function Display({ route }) {
-    let dataObj = route.params
+export default function App() {
+  const [message, setMessage] = useState('');
 
-    return (
-        <View style={ styless.container}>
-            <View style= {styless.header}>
-                <Text style={styless.headerText}>
-                    Your Resume
-                </Text>
-            </View>
-        <View style={styless.details}>
-            <Text style={styless.titleText}>
-                Personal Details
-            </Text>
-            <Text style={styless.text}>
-                <Text style={styless.key}> First Name : </Text>
-                <Text> {dataObj.firstName}</Text>
-            </Text>
+  const [updated, setUpdated] = useState(message);
 
-            <Text style={styless.text}>
-                <Text style={styless.key}> Last Name: </Text>
-                <Text> {dataObj.lastName}</Text>
-            </Text>
-        </View>
-        </View>
-    )
+  const handleChange = (event) => {
+    setMessage(event.target.value);
+  };
+
+  const handleClick = () => {
+    // 👇 "message" stores input field value
+    setUpdated(message);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        id="message"
+        name="message"
+        onChange={handleChange}
+        value={message}
+      />
+
+      <h2>Message: {message}</h2>
+
+      <h2>Updated: {updated}</h2>
+
+      <button onClick={handleClick}>Update</button>
+    </div>
+  );
 }
-
-const styless = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'grey',
-        paddingLeft: 40,
-        paddingRight: 40,
-        paddingTop: 40
-    },
-    header: {
-        marginBottom: 20,
-        alignSelf: 'stretch'
-    },
-    details: {
-        marginBottom: 15,
-    },
-    headerText: {
-        fontSize:  24,
-        color: '#fff',
-        borderBottomColor: '#199187',
-        paddingBottom: 10,
-        borderBottomWidth: 1
-    },
-    titleText: {
-        fontWeight: 'bold',
-        color: 'yellow',
-        fontSize: 15,
-        marginBottom: 10
-    },
-    key: {
-        fontWeight: 'bold'
-    },
-    text: {
-        color: '#d3d3d3',
-    }
-});
