d3f013cc634a41bee8e775d7cd85e73412cc5246
Ace <kronicrespawn@gmail.com>
2023-03-10 12:49:44 -0600

    refactored; form functional

--- src/component/education.js
+++ src/component/education.js
@@ -1,25 +1,105 @@
-import React, { Component } from 'react';
+import React from "react";
+    class App extends React.Component {
+      constructor(props) {
+        super();
+        this.state = {
+          sName: "",
+          degree: "",
+          sDate: "",
+          eDate: "",
+          data: {}
+        };
+      }
+    
+      handleChange = (e, type) => {
+        this.setState({ ...this.state, [type]: e.target.value });
+      };
+    
+      handleSubmit = (e) => {
+        e.preventDefault();
+        const { sName, degree, sDate, eDate } = this.state;
+        this.setState({
+          data: {
+            sName,
+            degree,
+            sDate,
+            eDate
+          }
+        });
+        console.log(this.state);
+      };
+      render() {
 
-class Education extends Component {
-  constructor(props) {
-    super(props);
-  }
+        return (
+          <div className="App">
+            <form onSubmit={this.handleSubmit}>
+              
+              <label>School Name</label>
+              <input
+                type="text"
+                value={this.state.sName}
+                onChange={(e) => {
+                  this.handleChange(e, "sName");
+                }}
+                name="sName"
+              />
+        
+              <br />
+              
+              <label>Degree</label>
+              <input
+                type="text"
+                value={this.state.degree}
+                onChange={(e) => {
+                  this.handleChange(e, "degree");
+                }}
+                name="degree"
+              />
+        
+              <br />
+             
+              <label>Start Date</label>
+              <input
+                type="sDate"
+                value={this.state.sDate}
+                onChange={(e) => {
+                  this.handleChange(e, "sDate");
+                }}
+                name="sDate"
+              />
+        
+              <br />
 
-  render() {
-    return (
-      <div>
-        <h3>{this.props.title}</h3>
-        <ul>
-        <li>School Name</li>
-        <li>Title of Studies</li> 
-        <li>Date of Study</li> 
-        </ul>
-      </div>
-    );
-  }
-}
+              <label>End Date</label>
+              <input
+                type="number"
+                value={this.state.eDate}
+                onChange={(e) => {
+                  this.handleChange(e, "eDate");
+                }}
+                name="eDate"
+              />
+        
+              <br />
+              
+              <button type="submit">Submit</button>
+            
+            </form>
+            
+            <h2>School Name: {this.state.data.sName}</h2>
+            
+            <h2>Degree: {this.state.data.degree}</h2>
+            
+            <h2>Start Date: {this.state.data.sDate}</h2>
 
-export default Education;
+            <h2>End Date: {this.state.data.eDate}</h2>
+          
+          </div>
+        );
+      }
+    }
+    
+    export default App;
 
 // A section to add your 
 // educational experience 
