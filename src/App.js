34d51ae64ab4a5ffbd34dad94ba62bfc1579f751
Ace <kronicrespawn@gmail.com>
2023-03-10 12:19:30 -0600

    work experience section added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 // import * as React from 'react';
 import Personal from "./component/genInfo";
+import Experience from "./component/practicalExp";
 // import Display from "./component/Form/Display";
 // import { NavigationContainer } from "@react-navigation/native"
 // import { createNativeStackNavigator } from "@react-navigation/native-stack"
@@ -9,7 +10,10 @@ class App extends Component {
   render() {
     return (<div>
           <h1>CV : Resume Creator</h1>
+          <h3> Personal Information</h3>
           <Personal />
+          <h3> Work Experience</h3>
+          <Experience />
         
           </div>
     )
