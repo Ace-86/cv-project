9bdacab712d12a8bacddee578b5cc3179d84dcfa
Ace <kronicrespawn@gmail.com>
2023-03-16 15:39:39 -0500

    import bug; no display

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import Personal from "./component/genInfo";
+import { Personal } from "./component/genInfo";
 import Experience from "./component/practicalExp";
 import Education from "./component/education";
+import Displayform from "./component/display";
 
 class App extends Component {
   render() {
@@ -13,7 +14,8 @@ class App extends Component {
           <Experience />
           <h3> Education</h3>
           <Education />
-
+          <h3> Display </h3>
+          <Displayform />
         
           </div>
     )
